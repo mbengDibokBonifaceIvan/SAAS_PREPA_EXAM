18eb7571671871443a94c49018a3356e1096d14f
Ivan <mbengivan63@gmail.com>
2026-02-07 02:10:53 +0100

    Avant de deplacer docker-compose plus haut

--- IAM_Service/backend/src/main/java/com/ivan/backend/infrastructure/config/SecurityAuditorAware.java
+++ IAM_Service/backend/src/main/java/com/ivan/backend/infrastructure/config/SecurityAuditorAware.java
@@ -3,6 +3,7 @@ package com.ivan.backend.infrastructure.config;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -14,11 +15,22 @@ public class SecurityAuditorAware implements AuditorAware<String> {
     public Optional<String> getCurrentAuditor() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        if (authentication == null || !authentication.isAuthenticated()) {
-            return Optional.of("SYSTEM"); // Utile pour les actions automatiques ou l'onboarding public
+        // 1. On vérifie si l'auth existe, est authentifiée ET n'est pas anonyme
+        if (authentication == null ||
+                !authentication.isAuthenticated() ||
+                authentication instanceof org.springframework.security.authentication.AnonymousAuthenticationToken) {
+            return Optional.of("SYSTEM");
         }
 
-        // Retourne le 'subject' ou l'email contenu dans le JWT (getName() renvoie souvent le sub ou l'email selon ta config)
+        // 2. Extraction du JWT
+        // Dans SecurityAuditorAware.java
+        if (authentication.getPrincipal() instanceof Jwt jwt) {
+            // On stocke le SUB (UUID de Keycloak) pour la stabilité
+            return Optional.ofNullable(jwt.getSubject());
+        }
+
+        // Retourne le 'subject' ou l'email contenu dans le JWT (getName() renvoie
+        // souvent le sub ou l'email selon ta config)
         return Optional.ofNullable(authentication.getName());
     }
 }
